e3296e5b5fcd80aa7822dbe65ceafb406a3bcb78
Sajiva <sajiva.pradhan@gmail.com>
2016-02-25 23:39:23 -0600

    Changed SQL syntax format

--- NormalForms/src/nf/GenerateSQL.java
+++ NormalForms/src/nf/GenerateSQL.java
@@ -4,7 +4,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.List;
-import java.util.Map;
 
 public class GenerateSQL {
 
@@ -12,6 +11,7 @@ public class GenerateSQL {
 		FileWriter writer = new FileWriter("NF.sql");
 		writer.close();
 	}
+
     public static void writeToFile(String sqlQuery) {
         try {
             Files.write(Paths.get("NF.sql"), sqlQuery.getBytes(), StandardOpenOption.CREATE, StandardOpenOption.APPEND);
@@ -25,15 +25,13 @@ public class GenerateSQL {
 
         StringBuilder sqlQuery = new StringBuilder();
         sqlQuery.append("SELECT COUNT(*) \n" +
-                "\tFROM " + tableName + "\n" +
-                "\t\tWHERE ");
+                "FROM " + tableName + "\n" +
+                "WHERE ");
 
         for (int i = 0; i < candidateKey.size(); i++) {
-            sqlQuery.append(candidateKey.get(i) + " IS NULL");
+            sqlQuery.append(candidateKey.get(i) + " IS NULL\n");
             if (i < candidateKey.size() - 1)
-                sqlQuery.append(" OR ");
-            else
-                sqlQuery.append("; \n");
+                sqlQuery.append("\tOR ");
         }
 
         writeToFile("\n -- Check nulls in candidate key \n");
@@ -46,7 +44,7 @@ public class GenerateSQL {
 
         StringBuilder sqlQuery = new StringBuilder();
         sqlQuery.append("SELECT COUNT(*) \n" +
-                "\tFROM " + tableName + ";\n");
+                "FROM " + tableName + ";\n");
 
         writeToFile("\n -- Get total rows count \n");
         writeToFile(sqlQuery.toString());
@@ -58,7 +56,7 @@ public class GenerateSQL {
 
         StringBuilder sqlQuery = new StringBuilder();
         sqlQuery.append("SELECT COUNT(*) \n" +
-                "\tFROM \n" +
+                "FROM \n" +
                 "\t(SELECT DISTINCT ");
 
         for (int i = 0; i < candidateKey.size(); i++) {
@@ -69,7 +67,7 @@ public class GenerateSQL {
                 sqlQuery.append("\n");
         }
 
-        sqlQuery.append("\t\tFROM " + tableName + ") T1;\n");
+        sqlQuery.append("\tFROM " + tableName + ") T1;\n");
 
         writeToFile("\n -- Get distinct count \n");
         writeToFile(sqlQuery.toString());
@@ -81,7 +79,7 @@ public class GenerateSQL {
 
         StringBuilder sqlQuery = new StringBuilder();
         sqlQuery.append("SELECT COUNT(DISTINCT " + candidateKey + ") \n" +
-                "\tFROM " + tableName + ";\n");
+                "FROM " + tableName + ";\n");
 
         writeToFile("\n -- Get distinct count \n");
         writeToFile(sqlQuery.toString());
@@ -90,10 +88,11 @@ public class GenerateSQL {
     }
     
     public static String getTableExist(String tableName) {
+
 		StringBuilder sqlQuery = new StringBuilder();
 		sqlQuery.append("SELECT COUNT(*) \n" +
-				"\tFROM Tables \n" + 
-				"\tWHERE table_name=\'"+tableName + "\';\n");
+				"FROM Tables \n" +
+				"WHERE table_name = \'"+tableName + "\';\n");
 		
 		writeToFile("\n -- Get queried table\n");
 		writeToFile(sqlQuery.toString());
@@ -102,10 +101,12 @@ public class GenerateSQL {
 	}
     
     public static String getColumnAndTableExist(String tableName, String columnName) {
+
 		StringBuilder sqlQuery = new StringBuilder();
 		sqlQuery.append("SELECT COUNT(*) \n" + 
-				"\tFROM Columns \n" +
-				"\tWHERE column_name=\'"+columnName+"\' AND table_name=\'"+tableName+"\';\n");
+				"FROM Columns \n" +
+				"WHERE column_name = \'"+columnName+"\'\n" +
+                "\tAND table_name = \'"+tableName+"\';\n");
 		
 		writeToFile("\n -- Check table and columns\n");
 		writeToFile(sqlQuery.toString());
@@ -127,7 +128,7 @@ public class GenerateSQL {
                 sqlQuery.append("\n");
         }
 
-        sqlQuery.append("\t\tFROM " + originalTable + ";\n");
+        sqlQuery.append("\tFROM " + originalTable + ";\n");
 
         writeToFile("\n -- Create local temporary table \n");
         writeToFile(sqlQuery.toString());
@@ -136,6 +137,7 @@ public class GenerateSQL {
     }
 
     public static String getCountJoinTables(List<String> tableNames, List<List<String>> foreignKeys) {
+
         StringBuilder sqlQuery = new StringBuilder();
         sqlQuery.append("SELECT COUNT(*)\n" +
                 "FROM " + tableNames.get(0) + "\n");
